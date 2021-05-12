b15b5f66bf9417aa86b9bbe9d6adb313c13c3108
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-05-12 21:03:18 +0530

    fixed the test cases

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Config;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Codeat3\BladeVaadinIcons\BladeVaadinIconsServiceProvider;
 
@@ -45,6 +46,36 @@ class CompilesIconsTest extends TestCase
             SVG;
 
         $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_default_class_from_config()
+    {
+        Config::set('blade-vaadin-icons.class', 'awesome');
+
+        $result = svg('vaadin-abacus')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="awesome" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16" fill="currentColor"><path fill="currentColor" d="M0 0v16h16v-16h-16zM14 2v3h-0.1c-0.2-0.6-0.8-1-1.4-1s-1.2 0.4-1.4 1h-3.2c-0.2-0.6-0.7-1-1.4-1s-1.2 0.4-1.4 1h-0.2c-0.2-0.6-0.7-1-1.4-1s-1.2 0.4-1.4 1h-0.1v-3h12zM13.9 10c-0.2-0.6-0.8-1-1.4-1s-1.2 0.4-1.4 1h-0.2c-0.2-0.6-0.8-1-1.4-1s-1.2 0.4-1.4 1h-3.2c-0.2-0.6-0.7-1-1.4-1s-1.2 0.4-1.4 1h-0.1v-4h0.1c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h0.2c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h3.2c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h0.1l-0.1 4zM2 14v-3h0.1c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h3.2c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h0.2c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h0.1v3h-12z"></path></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+
+    }
+
+    /** @test */
+    public function it_can_merge_default_class_from_config()
+    {
+        Config::set('blade-vaadin-icons.class', 'awesome');
+
+        $result = svg('vaadin-abacus', 'w-6 h-6')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="awesome w-6 h-6" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16" fill="currentColor"><path fill="currentColor" d="M0 0v16h16v-16h-16zM14 2v3h-0.1c-0.2-0.6-0.8-1-1.4-1s-1.2 0.4-1.4 1h-3.2c-0.2-0.6-0.7-1-1.4-1s-1.2 0.4-1.4 1h-0.2c-0.2-0.6-0.7-1-1.4-1s-1.2 0.4-1.4 1h-0.1v-3h12zM13.9 10c-0.2-0.6-0.8-1-1.4-1s-1.2 0.4-1.4 1h-0.2c-0.2-0.6-0.8-1-1.4-1s-1.2 0.4-1.4 1h-3.2c-0.2-0.6-0.7-1-1.4-1s-1.2 0.4-1.4 1h-0.1v-4h0.1c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h0.2c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h3.2c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h0.1l-0.1 4zM2 14v-3h0.1c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h3.2c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h0.2c0.2 0.6 0.8 1 1.4 1s1.2-0.4 1.4-1h0.1v3h-12z"></path></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+
     }
 
     protected function getPackageProviders($app)
