630c42b14c9c5c5b8ddb308e292990dd07d4da16
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2021-09-30 02:44:33 +0000

    Fix styling

--- src/BladeVaadinIconsServiceProvider.php
+++ src/BladeVaadinIconsServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Codeat3\BladeVaadinIcons;
 
 use BladeUI\Icons\Factory;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
 
 final class BladeVaadinIconsServiceProvider extends ServiceProvider
 {
